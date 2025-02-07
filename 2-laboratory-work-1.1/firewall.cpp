42492354431551ae1d0dc63eb38cd88acaff7b1e
Danil Domnich <domni4.d2@yandex.ru>
2025-02-07 23:54:20 +0300

    Иницилизировать класс firewall

---
+++ 2-laboratory-work-1.1/firewall.cpp
@@ -0,0 +1,12 @@
+﻿#include "firewall.hpp"
+
+Firewall::Firewall(const string& name, unsigned char security_status)
+{
+	this->name = name;
+	this->security_status = security_status;
+}
+
+bool Firewall::is_valid(unsigned char security_status) const
+{
+	return this->security_status <= security_status;
+}
---
+++ 2-laboratory-work-1.1/firewall.hpp
@@ -0,0 +1,19 @@
+﻿#ifndef FIREWALL_HPP
+#define FIREWALL_HPP
+
+#include <string>
+
+using namespace std;
+
+class Firewall
+{
+private:
+	string name;
+	unsigned char security_status;
+
+public:
+	Firewall(const string&, unsigned char);
+	bool is_valid(unsigned char) const;
+};
+
+#endif
